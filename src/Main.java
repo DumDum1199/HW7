a41008c967152b794da31b6612e4a736c81a54c3
Dmitrii <bobrovdmitrii00@mail.ru>
2025-08-18 18:58:01 +0300

    HW7

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,80 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Задача 1//
+        int firstFriday = 5;
+        for (int i = firstFriday;i <= 31; i += 7) {
+            System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет");
         }
+
+        //Задача 2//
+        int distance = 0;
+        int allDistance = 42195;
+        do {
+            distance +=500;
+            int a = allDistance-distance;
+            System.out.println("Держитесь! Осталось "+a+"метров");
+        } while (distance<(allDistance-500));
+
+        for (;distance <= allDistance; distance += 500) {
+            int b = allDistance-distance;
+            System.out.println("Держитесь! Осталось "+b+"метров");
+        }
+
+        //Задача 3//
+        int money = 2700;
+        int days = 1;
+        int dailyPayment = 100;
+        while (money>dailyPayment){
+            days++;
+            if(days%5==0){
+                continue;
+            }
+            money-=dailyPayment;
+        }
+        System.out.println(days-1);
+
+        money=2700;
+        for (days=1;money>dailyPayment;days++){
+            if(days%5==0){
+                continue;
+            }
+            money-=dailyPayment;
+        }
+        System.out.println(days-1);
+
+        //Задача 4//
+        int month = 0;
+        int total = 0;
+        do{
+            total+=15000;
+            month++;
+            if(month%6==0){
+                total+=total*0.07;
+            }
+           if(total>=12000000){
+                break;
+            }
+        }while(true);
+        System.out.println("Для накопления 12000000 нужно "+month+" месяцев");
+
+
+        //Задача 5//
+        int charge = 20;
+        int minute = 0;
+        int overheats = 0;
+        while (charge<100){
+            minute++;
+            if(minute%10==0){
+                overheats++;
+                continue;
+            }
+            if(overheats==3){
+                break;
+            }
+            charge+=2;
+        }
+        System.out.println("Время зарядки составило "+minute+" минут");
     }
 }
